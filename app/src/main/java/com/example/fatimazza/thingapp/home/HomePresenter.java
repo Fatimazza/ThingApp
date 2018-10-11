67638233ac10755315e3e2be55f1ffea36857b7a
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-10-11 23:57:10 +0700

    Network, Base MVP - Add API Service in Base Presenter

--- app/src/main/java/com/example/fatimazza/thingapp/home/HomePresenter.java
+++ app/src/main/java/com/example/fatimazza/thingapp/home/HomePresenter.java
@@ -1,8 +1,12 @@
 package com.example.fatimazza.thingapp.home;
 
 import com.example.fatimazza.thingapp.base.BasePresenter;
+import com.example.fatimazza.thingapp.network.ProductAPIServices;
 
 public class HomePresenter extends BasePresenter<HomeContract.View>
         implements HomeContract.Presenter {
 
+    public HomePresenter(ProductAPIServices productAPIServices) {
+        super(productAPIServices);
+    }
 }
