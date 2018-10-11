9fe50c294a9f0fd9eea9c7b03d1afd96f6f687b3
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-10-11 21:31:03 +0700

    Home - Init components, Add method to show loading, show data, show error

--- app/src/main/java/com/example/fatimazza/thingapp/home/HomeActivity.java
+++ app/src/main/java/com/example/fatimazza/thingapp/home/HomeActivity.java
@@ -1,19 +1,53 @@
 package com.example.fatimazza.thingapp.home;
 
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.example.fatimazza.thingapp.R;
 import com.example.fatimazza.thingapp.base.BaseActivity;
 
+import butterknife.BindView;
+
 public class HomeActivity extends BaseActivity implements HomeContract.View {
+
+    @BindView(R.id.rv_products)
+    public RecyclerView rvListOfProducts;
+
+    @BindView(R.id.tv_error_message_display)
+    public TextView tvErrorMsg;
+
+    @BindView(R.id.pb_loading_indicator)
+    public ProgressBar pbLoadingIndicator;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        showLoading();
     }
 
     @Override
     public int getLayout() {
         return R.layout.activity_home;
     }
+
+    private void showLoading() {
+        pbLoadingIndicator.setVisibility(View.VISIBLE);
+        rvListOfProducts.setVisibility(View.GONE);
+    }
+
+    private void showListOfProducts() {
+        tvErrorMsg.setVisibility(View.GONE);
+        rvListOfProducts.setVisibility(View.VISIBLE);
+    }
+
+    private void showErrorMessage() {
+        tvErrorMsg.setVisibility(View.VISIBLE);
+        rvListOfProducts.setVisibility(View.GONE);
+    }
+
 }
