85fb076f9a4156085b8089b57672c08ccd718172
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-10-11 22:26:22 +0700

    Home Adapter - Init components in View Holder

--- app/src/main/java/com/example/fatimazza/thingapp/home/HomeAdapter.java
+++ app/src/main/java/com/example/fatimazza/thingapp/home/HomeAdapter.java
@@ -3,6 +3,14 @@ package com.example.fatimazza.thingapp.home;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.example.fatimazza.thingapp.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.HomeAdapterViewHolder>{
 
@@ -28,8 +36,24 @@ public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.HomeAdapterVie
 
     class HomeAdapterViewHolder extends RecyclerView.ViewHolder {
 
+        View rootView;
+
+        @BindView(R.id.iv_product_thumbnail)
+        ImageView ivProductThumb;
+
+        @BindView(R.id.tv_product_title)
+        TextView tvProductTitle;
+
+        @BindView(R.id.tv_product_price)
+        TextView tvProductPrice;
+
+        @BindView(R.id.tv_product_desc)
+        TextView tvProductDesc;
+
         public HomeAdapterViewHolder(View itemView) {
             super(itemView);
+            ButterKnife.bind(this, itemView);
+            this.rootView = itemView;
         }
         
     }
