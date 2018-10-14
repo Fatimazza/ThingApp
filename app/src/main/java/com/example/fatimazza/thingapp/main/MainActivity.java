3be2b865ab9b5a6e0e20feac013d927d893984cd
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-10-14 14:52:36 +0700

    Main MVP - Add new Home activity to implement Fragment MVP

---
+++ app/src/main/java/com/example/fatimazza/thingapp/main/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.fatimazza.thingapp.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.fatimazza.thingapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
