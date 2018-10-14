0d45941d6dc6ecfba37f5c3c93b3a14ef5447d96
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-10-14 15:32:15 +0700

    Main - Add method to set Fragment

--- app/src/main/java/com/example/fatimazza/thingapp/main/MainActivity.java
+++ app/src/main/java/com/example/fatimazza/thingapp/main/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import com.example.fatimazza.thingapp.R;
 import com.example.fatimazza.thingapp.base.BaseActivity;
+import com.example.fatimazza.thingapp.base.BaseFragment;
 
 public class MainActivity extends BaseActivity implements MainContract.View {
 
@@ -17,4 +18,18 @@ public class MainActivity extends BaseActivity implements MainContract.View {
         return R.layout.activity_main;
     }
 
+
+    /**
+     * sets a new fragment on fragment_container frame
+     * layout and attaches to it the presenter
+     * @param fragment
+     */
+    @Override
+    public void setFragment(BaseFragment fragment) {
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
+    }
 }
--- app/src/main/java/com/example/fatimazza/thingapp/main/MainContract.java
+++ app/src/main/java/com/example/fatimazza/thingapp/main/MainContract.java
@@ -1,11 +1,13 @@
 package com.example.fatimazza.thingapp.main;
 
 import com.example.fatimazza.thingapp.base.BaseContractView;
+import com.example.fatimazza.thingapp.base.BaseFragment;
 
 public interface MainContract {
 
     interface View extends BaseContractView {
         
+        void setFragment(BaseFragment fragment);
 
     }
 
