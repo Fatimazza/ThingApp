f08213956ba2b0fcff5dc01378824fc18cdca4c2
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-10-14 19:38:09 +0700

    Product Fragment, Add Products Fragment MVP - Add Contract

---
+++ app/src/main/java/com/example/fatimazza/thingapp/productadd/AddProductFragmentContract.java
@@ -0,0 +1,15 @@
+package com.example.fatimazza.thingapp.productadd;
+
+import com.example.fatimazza.thingapp.base.BaseContractView;
+
+public interface AddProductFragmentContract {
+
+    interface View extends BaseContractView {
+
+    }
+
+    interface Presenter {
+
+    }
+
+}
---
+++ app/src/main/java/com/example/fatimazza/thingapp/productlist/ProductFragmentContract.java
@@ -0,0 +1,14 @@
+package com.example.fatimazza.thingapp.productlist;
+
+import com.example.fatimazza.thingapp.base.BaseContractView;
+
+public interface ProductFragmentContract {
+
+    interface View extends BaseContractView {
+
+    }
+
+    interface Presenter {
+
+    }
+}
