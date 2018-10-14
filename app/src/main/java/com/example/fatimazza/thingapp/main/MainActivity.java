653f16729173bcf4e115f8e6016c8ac4da1d184f
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-10-14 14:57:08 +0700

    Main MVP - Add Contract, Extending Base Activity

--- app/src/main/java/com/example/fatimazza/thingapp/main/MainActivity.java
+++ app/src/main/java/com/example/fatimazza/thingapp/main/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.fatimazza.thingapp.main;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.fatimazza.thingapp.R;
+import com.example.fatimazza.thingapp.base.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity implements MainContract.View {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public int getLayout() {
+        return R.layout.activity_main;
+    }
+
 }
---
+++ app/src/main/java/com/example/fatimazza/thingapp/main/MainContract.java
@@ -0,0 +1,16 @@
+package com.example.fatimazza.thingapp.main;
+
+import com.example.fatimazza.thingapp.base.BaseContractView;
+
+public interface MainContract {
+
+    interface View extends BaseContractView {
+        
+
+    }
+
+    interface Presenter {
+
+    }
+}
+
