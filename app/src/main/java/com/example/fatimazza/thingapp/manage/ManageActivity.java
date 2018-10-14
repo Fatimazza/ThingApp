9194ca06aedbac17cae6e030d8bbeb1de6ab5aa7
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-10-14 11:18:09 +0700

    Manage MVP - Add Contract, Init components, Add method to Add Product

--- app/src/main/java/com/example/fatimazza/thingapp/manage/ManageActivity.java
+++ app/src/main/java/com/example/fatimazza/thingapp/manage/ManageActivity.java
@@ -1,12 +1,26 @@
 package com.example.fatimazza.thingapp.manage;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.fatimazza.thingapp.R;
 import com.example.fatimazza.thingapp.base.BaseActivity;
 
-public class ManageActivity extends BaseActivity {
+import butterknife.BindView;
+
+public class ManageActivity extends BaseActivity implements ManageContract.View {
+
+    @BindView(R.id.et_name)
+    public EditText etProductName;
+
+    @BindView(R.id.et_price)
+    public EditText etProductPrice;
+
+    @BindView(R.id.et_desc)
+    public EditText etProductDesc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +30,15 @@ public class ManageActivity extends BaseActivity {
     @Override
     public int getLayout() {
         return R.layout.activity_manage;
+    }
+
+    public void addThing (View view) {
+        if (TextUtils.isEmpty(etProductName.getText().toString())
+                || TextUtils.isEmpty(etProductPrice.getText().toString())
+                || TextUtils.isEmpty(etProductDesc.getText().toString())) {
+            Toast.makeText(this, "Please fill in all data", Toast.LENGTH_LONG).show();
+        } else {
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/fatimazza/thingapp/manage/ManageContract.java
@@ -0,0 +1,14 @@
+package com.example.fatimazza.thingapp.manage;
+
+import com.example.fatimazza.thingapp.base.BaseContractView;
+
+public interface ManageContract {
+
+    interface View extends BaseContractView {
+
+    }
+
+    interface Presenter {
+
+    }
+}
